7aa637da239289f3b104691425f083d0a4c37a10
Jessie <farr.slmn@gmail.com>
2016-07-17 17:57:23 +0300

    Hi

--- src/ru/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
+++ src/ru/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
@@ -2,6 +2,6 @@ package ru.innopolis.university.bootcamp2016.programmingA.texasholdem;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("NNJ");
     }
 }
